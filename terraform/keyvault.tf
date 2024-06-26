41bf614b75773c29efcded83cc0208894e0f1c64
Hemen Sarzali <hemen.sarzali@atea.no>
2024-06-26 21:48:52 +0200

    Update KV name

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,6 +1,6 @@
 ## KeyVault
 resource "azurerm_key_vault" "tfazkv" {
-  name                = "${var.prefix}-kv-${var.env}-01"
+  name                = "${var.prefix}-kv01-${var.env}"
   location            = var.location
   resource_group_name = azurerm_resource_group.tfazrg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
