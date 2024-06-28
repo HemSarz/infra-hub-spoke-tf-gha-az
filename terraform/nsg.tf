542f0f932f8c02eabd2c0fcd341f9b568defe62d
Hemen Sarzali <hemen.sarzali@atea.no>
2024-06-28 12:28:36 +0200

    Reconfigured networking, KC secrets VM access

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -1,44 +1,33 @@
-## NSG for SSH | Web app VM
-resource "azurerm_network_security_group" "sshAllow" {
+## NSG for RDP | MGMT VM
+resource "azurerm_network_security_group" "RDPDeny" {
   name                = "${var.prefix}-nsg-webapp-intf-${var.env}"
   location            = var.location
   resource_group_name = azurerm_resource_group.tfazrg.name
 
   security_rule {
-    name                       = "${var.prefix}-ssh-rule-${var.env}"
+    name                       = "${var.prefix}-RDP-nsg-mgmt-vm-${var.env}"
     priority                   = 100
     direction                  = "Inbound"
-    access                     = "Allow"
+    access                     = "Deny"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "3389"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
 
-## NSG Association
-resource "azurerm_subnet_network_security_group_association" "sshAllowAssoc" {
-  subnet_id                 = azurerm_subnet.spokesub01.id
-  network_security_group_id = azurerm_network_security_group.sshAllow.id
+## NSG Association MGMT
+resource "azurerm_subnet_network_security_group_association" "RDPDeny" {
+  subnet_id                 = azurerm_subnet.spokesub02.id
+  network_security_group_id = azurerm_network_security_group.RDPDeny.id
 }
 
-resource "azurerm_network_security_group" "mgmt_vm_nsg" {
-  name                = "${var.prefix}-nsg-mgmt-vm-${var.env}"
+#### NSG for RDP | Web app VM
+resource "azurerm_network_security_group" "RDPAllow" {
+  name                = "${var.prefix}-nsg-web-vm-${var.env}"
   location            = var.location
   resource_group_name = azurerm_resource_group.tfazrg.name
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 
   security_rule {
     name                       = "Allow-RDP"
@@ -51,33 +40,10 @@ resource "azurerm_network_security_group" "mgmt_vm_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-  security_rule {
-    name                       = "Deny-All-Inbound"
-    priority                   = 4000
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-All-Outbound"
-    priority                   = 1000
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
-resource "azurerm_network_interface_security_group_association" "mgmt_vm_nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.mgmt_vm.id
-  network_security_group_id = azurerm_network_security_group.mgmt_vm_nsg.id
+resource "azurerm_subnet_network_security_group_association" "web_vm_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.spokesub01.id
+  network_security_group_id = azurerm_network_security_group.RDPDeny.id
+
 }
